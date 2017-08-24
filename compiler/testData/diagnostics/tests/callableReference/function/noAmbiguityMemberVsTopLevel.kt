96f056869b6d15c76c6e60bb5ddbc77e6ded99d1
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-08-24 17:15:42 +0300

    Support "::foo" as a short-hand for "this::foo"
     #KT-15667 Fixed

--- compiler/testData/diagnostics/tests/callableReference/function/noAmbiguityMemberVsTopLevel.kt
+++ compiler/testData/diagnostics/tests/callableReference/function/noAmbiguityMemberVsTopLevel.kt
@@ -1,4 +1,5 @@
 // !CHECK_TYPE
+// !LANGUAGE: +CallableReferencesToClassMembersWithEmptyLHS
 
 import kotlin.reflect.KFunction0
 
@@ -11,14 +12,14 @@ class A {
     fun foo() {}
     
     fun main() {
-        val x = ::<!CALLABLE_REFERENCE_TO_MEMBER_OR_EXTENSION_WITH_EMPTY_LHS!>foo<!>
+        val x = ::foo
 
         checkSubtype<KFunction0<Unit>>(x)
 
         explicitlyExpectFunction0(x)
         explicitlyExpectFunction1(<!TYPE_MISMATCH!>x<!>)
 
-        explicitlyExpectFunction0(::<!CALLABLE_REFERENCE_TO_MEMBER_OR_EXTENSION_WITH_EMPTY_LHS!>foo<!>)
-        explicitlyExpectFunction1(<!TYPE_MISMATCH!>::<!CALLABLE_REFERENCE_TO_MEMBER_OR_EXTENSION_WITH_EMPTY_LHS!>foo<!><!>)
+        explicitlyExpectFunction0(::foo)
+        explicitlyExpectFunction1(<!TYPE_MISMATCH!>::foo<!>)
     }
 }
